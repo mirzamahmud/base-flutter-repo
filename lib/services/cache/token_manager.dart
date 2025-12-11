9f9cf5929d111fa440ca80a11af8e9b35158641e
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-12-11 14:10:37 +0600

    remove token manager

--- lib/services/cache/token_manager.dart
+++
@@ -1,7 +0,0 @@
-abstract interface class TokenManager {
-  Future<String?> getAccessToken();
-  Future<String?> getRefreshToken();
-  Future<void> saveAccessToken(String token);
-  Future<void> saveRefreshToken(String token);
-  Future<void> clearToken();
-}
